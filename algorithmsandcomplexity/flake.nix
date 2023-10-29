a51d3cfdec7d23fdffc23f0e438aa9638fbbddeb
Petros Angelatos <petrosagg@gmail.com>
2023-10-29 15:54:36 +0200

    flake stuff

--- algorithmsandcomplexity/flake.nix
+++ algorithmsandcomplexity/flake.nix
@@ -9,7 +9,11 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk nicematrix pgf tools amsmath epstopdf epstopdf-pkg infwarerr grfext kvdefinekeys kvoptions ltxcmds kvsetkeys xetex oberdiek cite colortbl hyperref bookmark mathtools listings import algorithms float algorithmicx varwidth enumitem thmtools footmisc caption;
+        inherit (pkgs.texlive) scheme-minimal latex-bin latexmk xetex
+        algorithmicx algorithms amsmath babel-english bookmark caption cite
+        colortbl enumitem epstopdf epstopdf-pkg float footmisc grfext hyperref
+        import infwarerr kvdefinekeys kvoptions kvsetkeys listings ltxcmds
+        mathtools nicematrix oberdiek pgf thmtools tools varwidth;
       };
     in rec {
       packages = {
@@ -29,7 +33,7 @@
           '';
           installPhase = ''
             mkdir -p $out
-            cp document.pdf $out/
+            cp main.pdf $out/
           '';
         };
       };
